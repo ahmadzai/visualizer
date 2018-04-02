425072fab5e87393e289174a63556ebbef847dde
khan <wazir.ahmadzai@gmail.com>
2018-04-02 11:00:01 +0430

    Heading text fixed in the ICN page

---
+++ src/AppBundle/Entity/IntOdkSmMonitoring.php
@@ -0,0 +1,426 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AdminData
+ *
+ * @ORM\Table(name="odk_sm_monitoring")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\OdkSmRepository")
+ */
+class OdkSmMonitoring
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @var \AppBundle\Entity\District
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\District", fetch="EXTRA_LAZY")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="district", referencedColumnName="id")
+     * })
+     */
+    private $district;
+
+    /**
+     * @ORM\Column(type="string", name="cluster")
+     */
+    private $cluster;
+
+    /**
+     * @ORM\Column(type="text", name="monitor_name")
+     */
+    private $monitorName;
+
+    /**
+     * @ORM\Column(type="text", name="ccs_name")
+     */
+    private $ccsName;
+
+    /**
+     * @ORM\Column(type="text", name="sm_name")
+     */
+    private $smName;
+
+    /**
+     * @ORM\Column(type="string", name="campaign_phase")
+     */
+    private $campaignPhase;
+
+    /**
+     * @ORM\Column(type="integer", name="attendance")
+     */
+    private $attendance;
+
+    /**
+     * @ORM\Column(type="integer", name="profile", length=2)
+     */
+    private $profile;
+
+    /**
+     * @ORM\Column(type="integer", name="preparedness")
+     */
+    private $preparedness;
+
+    /**
+     * @ORM\Column(type="integer", name="tracking_missed")
+     */
+    private $trackingMissed;
+
+    /**
+     * @ORM\Column(type="integer", name="tallying")
+     */
+    private $tallying;
+
+    /**
+     * @ORM\Column(type="integer", name="mobilization")
+     */
+    private $mobilization;
+
+    /**
+     * @ORM\Column(type="integer", name="advocacy")
+     */
+    private $advocacy;
+
+    /**
+     * @ORM\Column(type="integer", name="iec_material")
+     */
+    private $iecMaterial;
+
+    /**
+     * @ORM\Column(type="integer", name="higher_supv")
+     */
+    private $higherSupv;
+
+    /**
+     * @ORM\Column(type="integer", name="refusal_challenge")
+     */
+    private $refusalChallenge;
+
+    /**
+     * @ORM\Column(type="integer", name="access_challenge")
+     */
+    private $accessChallenge;
+
+    /**
+     * @ORM\Column(type="date", name="monitoring_date")
+     */
+    private $monitoringDate;
+
+    /**
+     * @return mixed
+     */
+    public function getCluster()
+    {
+        return $this->cluster;
+    }
+
+    /**
+     * @param mixed $cluster
+     */
+    public function setCluster($cluster)
+    {
+        $this->cluster = $cluster;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonitorName()
+    {
+        return $this->monitorName;
+    }
+
+    /**
+     * @param mixed $monitorName
+     */
+    public function setMonitorName($monitorName)
+    {
+        $this->monitorName = $monitorName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCcsName()
+    {
+        return $this->ccsName;
+    }
+
+    /**
+     * @param mixed $ccsName
+     */
+    public function setCcsName($ccsName)
+    {
+        $this->ccsName = $ccsName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSmName()
+    {
+        return $this->smName;
+    }
+
+    /**
+     * @param mixed $smName
+     */
+    public function setSmName($smName)
+    {
+        $this->smName = $smName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCampaignPhase()
+    {
+        return $this->campaignPhase;
+    }
+
+    /**
+     * @param mixed $campaignPhase
+     */
+    public function setCampaignPhase($campaignPhase)
+    {
+        $this->campaignPhase = $campaignPhase;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttendance()
+    {
+        return $this->attendance;
+    }
+
+    /**
+     * @param mixed $attendance
+     */
+    public function setAttendance($attendance)
+    {
+        $this->attendance = $attendance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfile()
+    {
+        return $this->profile;
+    }
+
+    /**
+     * @param mixed $profile
+     */
+    public function setProfile($profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreparedness()
+    {
+        return $this->preparedness;
+    }
+
+    /**
+     * @param mixed $preparedness
+     */
+    public function setPreparedness($preparedness)
+    {
+        $this->preparedness = $preparedness;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrackingMissed()
+    {
+        return $this->trackingMissed;
+    }
+
+    /**
+     * @param mixed $trackingMissed
+     */
+    public function setTrackingMissed($trackingMissed)
+    {
+        $this->trackingMissed = $trackingMissed;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTallying()
+    {
+        return $this->tallying;
+    }
+
+    /**
+     * @param mixed $tallying
+     */
+    public function setTallying($tallying)
+    {
+        $this->tallying = $tallying;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getMobilization()
+    {
+        return $this->mobilization;
+    }
+
+    /**
+     * @param mixed $mobilization
+     */
+    public function setMobilization($mobilization)
+    {
+        $this->mobilization = $mobilization;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdvocacy()
+    {
+        return $this->advocacy;
+    }
+
+    /**
+     * @param mixed $advocacy
+     */
+    public function setAdvocacy($advocacy)
+    {
+        $this->advocacy = $advocacy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIecMaterial()
+    {
+        return $this->iecMaterial;
+    }
+
+    /**
+     * @param mixed $iecMaterial
+     */
+    public function setIecMaterial($iecMaterial)
+    {
+        $this->iecMaterial = $iecMaterial;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHigherSupv()
+    {
+        return $this->higherSupv;
+    }
+
+    /**
+     * @param mixed $higherSupv
+     */
+    public function setHigherSupv($higherSupv)
+    {
+        $this->higherSupv = $higherSupv;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefusalChallenge()
+    {
+        return $this->refusalChallenge;
+    }
+
+    /**
+     * @param mixed $refusalChallenge
+     */
+    public function setRefusalChallenge($refusalChallenge)
+    {
+        $this->refusalChallenge = $refusalChallenge;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccessChallenge()
+    {
+        return $this->accessChallenge;
+    }
+
+    /**
+     * @param mixed $accessChallenge
+     */
+    public function setAccessChallenge($accessChallenge)
+    {
+        $this->accessChallenge = $accessChallenge;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonitoringDate()
+    {
+        return \DateTimeImmutable::createFromFormat('Y-m-d',$this->monitoringDate);
+    }
+
+    /**
+     * @param mixed $monitoringDate
+     */
+    public function setMonitoringDate($monitoringDate)
+    {
+        $this->monitoringDate = \DateTimeImmutable::createFromFormat('Y-m-d',$monitoringDate);
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Set district
+     *
+     * @param \AppBundle\Entity\District $district
+     *
+     * @return OdkSmMonitoring
+     */
+    public function setDistrict(\AppBundle\Entity\District $district = null)
+    {
+        $this->district = $district;
+
+        return $this;
+    }
+
+    /**
+     * Get district
+     *
+     * @return \AppBundle\Entity\District
+     */
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+}
---
+++ src/AppBundle/Repository/IntOdkCcsRepository.php
@@ -0,0 +1,414 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wakhan
+ * Date: 1/23/2018
+ * Time: 8:22 PM
+ */
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+class OdkCcsRepository extends EntityRepository {
+
+    /**
+     * @param $function
+     * @param $parameters
+     * @param null $secondParam
+     * @return mixed
+     */
+    public function callMe($function, $parameters, $secondParam = null) {
+        return call_user_func(array($this, $function), $parameters, $secondParam);
+    }
+
+    protected static $DQL = "   count(ccs.id) as times,
+                                avg(ccs.attendance) as attendance, 
+                                avg(ccs.profile) as profile, 
+                                avg(ccs.preparedness) as preparedness, 
+                                avg(ccs.mentoring) as mentoring, 
+                                avg(ccs.trackingMissed) as trackingMissed, 
+                                avg(ccs.planningReview) as planningReview, 
+                                avg(ccs.mobilization) as mobilization, 
+                                avg(ccs.advocacy) as advocacy, 
+                                avg(ccs.iecMaterial) as iecMaterial,
+                                avg(ccs.higherSupv) as higherSup,
+                                avg(ccs.refusalChallenge) as refusalChallenge,
+                                avg(ccs.accessChallenge) as accessChallenge ";
+
+    /**
+     * @param $districts
+     * @param $monthsYear
+     * @return mixed
+     */
+    public function clustersByDistrictMonth($districts, $monthsYear) {
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode,
+                d.id as dcode, d.districtName, ccs.cluster as clusterNo
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND ccs.district IN (:dist)
+                 AND ccs.cluster IS NOT NULL
+                GROUP BY ccs.cluster ORDER BY d.id, ccs.cluster
+                "
+            ) ->setParameters(['year'=>$years, 'month'=>$months, 'dist'=> $districts])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /*
+     * @param $region
+     * @param int $startDate
+     * @param int $endDate
+     * @return array
+     *
+    public function aggByProvince($region, $startDate = 0, $endDate = 0) {
+        // if end date and start date was not set
+        $endDate = $endDate === 0 ? $this->getLastDate() : $endDate;
+        $startDate = $startDate === 0  || $startDate === null ? $this->getStartDate($endDate) : $startDate;
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (ccs.monitoringDate Between :startDate AND :endDate)
+                 AND p.provinceRegion IN (:region)
+                GROUP BY p.id, mdate ORDER BY p.id, ccs.monitoringDate DESC
+                "
+            ) ->setParameters(['startDate'=>$startDate, 'endDate'=>$endDate, 'region'=>$region])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+    */
+
+
+    /**
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByMonthRegion($monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                GROUP BY p.id ORDER BY p.id
+                "
+            ) ->setParameters(['year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /**
+     * @param $region
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByMonthProvince($region, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND p.provinceRegion IN (:region)
+                GROUP BY p.id ORDER BY p.id
+                "
+            ) ->setParameters(['region'=>$region, 'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /**
+     * @param $province
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByMonthDistrict($province, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+               WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND d.province IN (:prov)
+                GROUP BY d.id ORDER BY p.id, d.id
+                "
+            ) ->setParameters(['prov'=>$province,'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+
+    /**
+     * @param $district
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByMonthCluster($district, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, ccs.cluster, ccs.monitorName, ccs.dcoName, ccs.ccsName,
+                ccs.campaignPhase, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND ccs.district IN (:dist)
+                 AND ccs.cluster IS NOT NULL
+                GROUP BY ccs.cluster ORDER BY d.id, ccs.cluster
+                "
+            ) ->setParameters(['dist'=>$district, 'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /**
+     * @param $distClusters ['cluster', 'district']
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByMonthCcs($distClusters, $monthsYear) {
+
+        $clusters = $distClusters['cluster'];
+        $dist = $distClusters['district'];
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, ccs.cluster, ccs.monitorName, ccs.dcoName, ccs.ccsName,
+                ccs.campaignPhase, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND ccs.district IN (:dist)
+                 AND ccs.cluster IN (:cluster)
+                GROUP BY ccs.cluster, ccs.ccsName ORDER BY d.id, ccs.cluster, ccs.dcoName
+                "
+            ) ->setParameters(['cluster'=>$clusters, 'year'=>$years, 'month'=>$months, 'dist'=>$dist])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+
+    /**
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByRegionMonth($monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                GROUP BY p.id, mdate ORDER BY p.id, ccs.monitoringDate DESC
+                "
+            ) ->setParameters(['year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+
+    /**
+     * @param $region
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByProvinceMonth($region, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND p.provinceRegion IN (:region)
+                GROUP BY p.id, mdate ORDER BY p.id, ccs.monitoringDate DESC
+                "
+            ) ->setParameters(['region'=>$region, 'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /**
+     * @param $province
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByDistrictMonth($province, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+               WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND d.province IN (:prov)
+                GROUP BY d.id, mdate ORDER BY p.id, d.id, ccs.monitoringDate DESC
+                "
+            ) ->setParameters(['prov'=>$province,'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+
+    /**
+     * @param $district
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByClusterMonth($district, $monthsYear) {
+
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, ccs.cluster, ccs.monitorName, ccs.dcoName, ccs.ccsName,
+                ccs.campaignPhase, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND ccs.district IN (:dist)
+                 AND ccs.cluster IS NOT NULL
+                GROUP BY ccs.cluster, mdate ORDER BY d.id, ccs.cluster, ccs.monitoringDate DESC
+                "
+            ) ->setParameters(['dist'=>$district, 'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+    /**
+     * @param $distClusters ['district', 'cluster']
+     * @param $monthsYear (Year-Month)
+     * @return mixed
+     */
+    public function aggByCcsMonth($distClusters, $monthsYear) {
+
+        $clusters = $distClusters['cluster'];
+        $dist = $distClusters['district'];
+        $months = $this->extractMonth($monthsYear);
+        $years = $this->extractYear($monthsYear);
+
+        return $this->getEntityManager()
+            ->createQuery(
+                "SELECT p.id as pcode, p.provinceRegion as region, p.provinceName as provinceName,
+                d.id as dcode, d.districtName as districtName, ccs.cluster, ccs.monitorName, ccs.dcoName, ccs.ccsName,
+                ccs.campaignPhase, 
+                ".self::$DQL.", Month(ccs.monitoringDate) as mdate, 
+                CONCAT(MonthName(ccs.monitoringDate),'-', YEAR(ccs.monitoringDate)) as mName
+                FROM AppBundle:OdkCcsMonitoring ccs JOIN ccs.district d JOIN d.province p
+                WHERE (YEAR(ccs.monitoringDate) IN (:year))
+                 AND (MONTH(ccs.monitoringDate) IN (:month))
+                 AND ccs.district IN (:dist)
+                 AND ccs.cluster IN (:cluster)
+                GROUP BY ccs.ccsName, ccs.cluster, mdate ORDER BY d.id, ccs.cluster, ccs.dcoName
+                "
+            ) ->setParameters(['dist'=>$dist, 'cluster'=>$clusters, 'year'=>$years, 'month'=>$months])
+            ->getResult(Query::HYDRATE_SCALAR);
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    private function getLastDate() {
+
+        $data = $this->getEntityManager()->createQuery(
+            "SELECT max(tbl.monitoringDate) as lastDate FROM AppBundle:OdkCcsMonitoring tbl"
+        )
+            ->getResult(Query::HYDRATE_SCALAR);
+
+        return $data[0]['lastDate'];
+    }
+
+    private function getStartDate($endDate) {
+        //return \Dat('Y-m-01', strtotime($endDate));
+        $year = date('Y', strtotime($endDate));
+        $month = date('m', strtotime($endDate));
+
+        $startDate = $year."-".$month."-"."01";
+
+        return $startDate;
+        //return \DateTimeImmutable::createFromFormat('Y-m-01',$endDate);
+    }
+
+    /**
+     * @param $monthsYear (Year-month)
+     * @return array months
+     */
+    private function extractMonth($monthsYear) {
+        $months = array();
+        foreach ($monthsYear as $item) {
+            $m = explode("-", $item);
+            $months[] = $m[1];
+        }
+
+        return $months;
+    }
+
+    /**
+     * @param $monthsYear (Year-month)
+     * @return array years
+     */
+    private function extractYear($monthsYear) {
+        $years = array();
+        foreach ($monthsYear as $item) {
+            $y = explode("-", $item);
+            $years[] = $y[0];
+        }
+
+        return $years;
+    }
+
+
+}
